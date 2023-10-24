78b3ab443a30822939f27a8ba56dcfc236b2f9ff
Viet Phong <phongkv@teracom.vn>
2023-10-24 11:56:27 +0700

    update appError

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -5,7 +5,5 @@ export default class AppError extends Error {
     super(message);
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
-
-    Error.captureStackTrace(this, this.constructor);
   }
 }
